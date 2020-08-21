e1ab85de9e479886704d2d82ce7d66d94f3cdfe3
shinder <shinder.lin@gmail.com>
2020-08-21 14:57:27 +0800

    plus api: ajax 2

--- test/a20200821-05-plus-api.php
+++ test/a20200821-05-plus-api.php
@@ -1,7 +1,10 @@
 <?php
 
-$a = intval($_GET['a']) ?? 0;
-$b = intval($_GET['b']) ?? 0;
+//$a = intval($_GET['a']) ?? 0;
+//$b = intval($_GET['b']) ?? 0;
+
+$a = isset($_GET['a']) ? intval($_GET['a']) : 0;
+$b = isset($_GET['b']) ? intval($_GET['b']) : 0;
 
 // echo '{"answer":'. ($a+$b). '}';
 printf('{"answer":%s}', $a+$b);
