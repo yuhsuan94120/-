9ec4c583dfb6d145006d9a54408de13d503e65a9
shinder <shinder.lin@gmail.com>
2020-08-25 11:54:53 +0800

    拆解頁面

---
+++ test/a20200825-08-home-test.php
@@ -0,0 +1,8 @@
+<?php require __DIR__. '/parts/__html_head.php'; ?>
+<?php include __DIR__. '/parts/__navbar.php'; ?>
+<div class="container">
+    <h2>Hello~</h2>
+
+</div>
+<?php include __DIR__. '/parts/__scripts.php'; ?>
+<?php include __DIR__. '/parts/__html_foot.php'; ?>
