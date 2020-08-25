7e17684fe423a6a8b022d1a1227b753b9d6e626e
shinder <shinder.lin@gmail.com>
2020-08-25 15:52:17 +0800

    while loop

--- test/a20200825-09-db-query.php
+++ test/a20200825-09-db-query.php
@@ -4,5 +4,7 @@ require __DIR__. '/parts/__connect_db.php';
 $stmt = $pdo->query("SELECT * FROM `address_book` LIMIT 5");
 
 echo json_encode($stmt->fetchAll(), JSON_UNESCAPED_UNICODE);
+// echo json_encode($stmt->fetchAll(PDO::FETCH_NUM), JSON_UNESCAPED_UNICODE);  // 索引式陣列
+// echo json_encode($stmt->fetchAll(PDO::FETCH_BOTH), JSON_UNESCAPED_UNICODE); // 給兩種: 索引式 + 關聯式
 
 
