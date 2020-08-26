0a6879bdd181250270542aed09ea752b55fc831d
shinder <shinder.lin@gmail.com>
2020-08-26 14:58:49 +0800

    新增資料的表單

--- test/data-insert.php
+++ test/data-insert.php
@@ -5,9 +5,49 @@ require __DIR__. '/parts/__connect_db.php';
 ?>
 <?php require __DIR__. '/parts/__html_head.php'; ?>
 <?php include __DIR__. '/parts/__navbar.php'; ?>
-    <div class="container">
-        <h2>insert</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
 
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">新增資料</h5>
+
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="name">name</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">email</label>
+                            <input type="email" class="form-control" id="email" name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="mobile">mobile</label>
+                            <input type="text" class="form-control" id="mobile" name="mobile">
+                        </div>
+                        <div class="form-group">
+                            <label for="birthday">birthday</label>
+                            <input type="date" class="form-control" id="birthday" name="birthday">
+                        </div>
+                        <div class="form-group">
+                            <label for="address">address</label>
+                            <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
     </div>
+
+
+
+
+
+
+</div>
 <?php include __DIR__. '/parts/__scripts.php'; ?>
 <?php include __DIR__. '/parts/__html_foot.php'; ?>
